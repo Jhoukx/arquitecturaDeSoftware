5351da839fa8a8a2f93f260ff1c1d92e366a0b6a
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-10-20 10:55:33 -0500

    refactor: :recycle: Controlador de cita aislado de las consultas a la base de datos

--- app/cita/aplicacion/cita.controller.js
+++ app/cita/aplicacion/cita.controller.js
@@ -1,4 +1,4 @@
-import { getAllCitas } from "../infraestructura/cita.mongodb.js";
+import { getAllCitas, getCitasDelPaciente, getPacientesDeMedico } from "../infraestructura/cita.mongodb.js";
 
 const getAll = async (req, res) => {
     try {
@@ -11,8 +11,8 @@ const getAll = async (req, res) => {
 
 const getPaciente = async (req, res) => {
     try {
-        let result = await cita.find({ datosUsuario: Number(req.params.id) }).toArray();
-        res.send(result)
+        let result = await getCitasDelPaciente(req.params.id);
+        res.json(result)
     } catch (error) {
         res.status(404).json({ status: 404, message: 'Couldn´t find a cita from that usuario' })
     }
@@ -20,45 +20,7 @@ const getPaciente = async (req, res) => {
 
 const getMedico = async (req, res) => {
     try {
-        let result = await cita.aggregate([
-            {
-                $match: {
-                    medico: Number(req.params.id)
-                }
-            },
-            {
-                $lookup: {
-                    from: 'usuario',
-                    localField: 'medico',
-                    foreignField: 'id',
-                    as: 'Pacientes'
-                }
-            },
-            {
-                $unwind: '$Pacientes'
-            },
-            {
-                $project: {
-                    ID: '$id',
-                    FECHA: '$fecha',
-                    ESTADO: '$estado',
-                    MEDICO: '$medico',
-                    DATOSUSUARIO: '$datosUsuario',
-                    PACIENTES: {
-                        ID: '$Pacientes.id',
-                        NOMBRE: '$Pacientes.nombre',
-                        PRIMER_APELLIDO: '$Pacientes.primer_apellido',
-                        SEGUNDO_APELLIDO: '$Pacientes.segundo_apellido',
-                        TELEFONO: '$Pacientes.telefono',
-                        DIRECCION: '$Pacientes.direccion',
-                        EMAIL: '$Pacientes.email',
-                        TIPODOC: '$Pacientes.tipodoc',
-                        GENERO: '$Pacientes.genero',
-                        ACUDIENTE: '$Pacientes.acudiente'
-                    }
-                }
-            }
-        ]).toArray()
+        let result = await getPacientesDeMedico(req.params.id);
         res.send(result)
     } catch (error) {
         res.status(404).json({ status: 404, message: 'Couldn´t find' })
--- app/cita/infraestructura/cita.mongodb.js
+++ app/cita/infraestructura/cita.mongodb.js
@@ -6,3 +6,55 @@ const cita = db.collection('cita');
 export const  getAllCitas =  async() => {
     return await cita.find().sort({ fecha: 1 }).toArray()
 }
+
+export const getCitasDelPaciente = async (id) => {
+    return await cita.aggregate([
+        {
+            $match: {
+                datosUsuario: Number(id)
+            }
+        }
+    ]).toArray();
+}
+
+export const getPacientesDeMedico = async (id) => {
+    return await cita.aggregate([
+        {
+            $match: {
+                medico: Number(id)
+            }
+        },
+        {
+            $lookup: {
+                from: 'usuario',
+                localField: 'medico',
+                foreignField: 'id',
+                as: 'Pacientes'
+            }
+        },
+        {
+            $unwind: '$Pacientes'
+        },
+        {
+            $project: {
+                ID: '$id',
+                FECHA: '$fecha',
+                ESTADO: '$estado',
+                MEDICO: '$medico',
+                DATOSUSUARIO: '$datosUsuario',
+                PACIENTES: {
+                    ID: '$Pacientes.id',
+                    NOMBRE: '$Pacientes.nombre',
+                    PRIMER_APELLIDO: '$Pacientes.primer_apellido',
+                    SEGUNDO_APELLIDO: '$Pacientes.segundo_apellido',
+                    TELEFONO: '$Pacientes.telefono',
+                    DIRECCION: '$Pacientes.direccion',
+                    EMAIL: '$Pacientes.email',
+                    TIPODOC: '$Pacientes.tipodoc',
+                    GENERO: '$Pacientes.genero',
+                    ACUDIENTE: '$Pacientes.acudiente'
+                }
+            }
+        }
+    ]).toArray()
+}
